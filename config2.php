5a24dc9be402802556ff93348a95c67de3d43f15
Mtsantos07 <mtsantos015@gmail.com>
2025-11-17 12:14:42 -0300

    Create config2.php

---
+++ config2.php
@@ -0,0 +1,20 @@
+config2.php
+<?php
+$host = 'localhost';
+$db   = 'escola';  // substitua pelo nome do banco
+$user = 'root';       // seu usuário
+$pass = '';           // sua senha
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+    die("Erro na conexão: " . $e->getMessage());
+}
+?>
